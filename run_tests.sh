471cfbab9c09a437426c7a75c74cb5d571d38f7f
Rob McColl <rob@robmccoll.com>
2013-05-17 11:20:07 -0400

    adding large results

--- run_tests.sh
+++ run_tests.sh
@@ -4,11 +4,25 @@ graphdir=graphs
 resultsdir=results
 
 # files
-graphs="tiny small medium large"
-tests=$(ls $testdir)
+if [ $# -ge 1 ]; then
+  graphs="$1"
+else
+  graphs="tiny small medium large"
+fi
+
+if [ $# -ge 2 ]; then
+  tests="$2"
+else
+  tests=$(ls $testdir)
+fi
 
 run=$(date "+%Y.%m.%d.%H.%M.%S")
 cwd=$(pwd)
+
+echo "Starting with config:"
+echo "  graphs: $graphs"
+echo "  frameworks: $tests"
+echo "  runID: $run"
 
 for g in $graphs
   do
@@ -16,11 +30,11 @@ for g in $graphs
       do
 	if [ -e $testdir/$f/runme.sh ]
 	  then
-	    echo "Running Graph: $g, Framework: $f..."
+	    echo "Running Graph: $g, Framework: $f Start Time: $(date '+%Y/%m/%d %H:%M:%S')..."
 	    outfile=$cwd/$resultsdir/$run.$f.$g
 	    sh sysinfo.sh > $outfile
 	    cd $testdir/$f; sh runme.sh $cwd/$graphdir/$g.g $cwd/$graphdir/$g.a >> $outfile; cd $cwd
-	    echo "-->done."
+	    echo "  done."
 	  fi
       done
   done
